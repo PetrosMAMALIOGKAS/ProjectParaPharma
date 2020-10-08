f3d2c88c2dd784fc42f7230bd24431017f9fe9e9
Petros MAMALIOGKAS <petros.mamaliogas@gmail.com>
2020-10-08 12:38:58 +0200

    Creation de classe ReadDataFrom file closes issue #5

---
+++ ProjectParaPharma/src/com/parapharma/analytics/ReadDataFromFile.java
@@ -0,0 +1,55 @@
+package com.parapharma.analytics;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ReadDataFromFile implements ISymptomReader {
+
+	private String filepath;
+	private ArrayList<String> contenu;
+	
+	public ReadDataFromFile() {
+	}
+	
+	public ReadDataFromFile(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	public String getFilepath() {
+		return this.filepath;
+	}
+	
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	public List<String> getSymptoms() {
+		return this.contenu;
+	}
+	
+	public void readFile() {
+		contenu = new ArrayList<String>();
+		
+		if (this.getFilepath() != null) {
+			try {
+				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				String line = reader.readLine();
+				
+				while (line != null) {
+					contenu.add(line);
+					line = reader.readLine();
+				}
+				reader.close();
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
